3a41e20cd76d9e11bc472de3455e0d33d04d6c35
maleekcodes <abdulthegeek@gmail.com>
2020-08-24 06:30:34 -0700

    React firestore

---
+++ src/components/App/index.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Navigation from '../Navigation';
+import Landing from '../Landing';
+import SignUp from '../SignUp';
+import SignIn from '../SignIn';
+import PasswordForget from '../PasswordForget';
+import Home from '../Home';
+import Account from '../Account';
+import Admin from '../Admin';
+
+import * as ROUTES from "../../constants/routes";
+import { withFirebase } from "../Firebase";
+import {AuthUserContext} from "../Session";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            authUser: null,
+        };
+    }
+
+    componentDidMount() {
+        this.props.firebase.auth.onAuthStateChanged(authUser => {
+            authUser 
+            ? this.setState({ authUser })
+            : this.setState({ authUser: null });
+        });
+    }
+
+    componentDidUnMount() {
+        this.listener();
+    }
+
+    render() {
+        return (
+            <AuthUserContext.Provider value={this.state.authUser}>
+                <Router>
+                    <Navigation />
+                    <hr />
+                    <Route exact path={ROUTES.LANDING} component={Landing} />
+                    <Route path={ROUTES.SIGN_UP} component={SignUp} />
+                    <Route path={ROUTES.SIGN_IN} component={SignIn} />
+                    <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
+                    <Route path={ROUTES.HOME} component={Home} />
+                    <Route path={ROUTES.ACCOUNT} component={Account} />
+                    <Route path={ROUTES.ADMIN} component={Admin} />
+                </Router>
+            </AuthUserContext.Provider>
+        );
+    }
+}
+
+
+export default withFirebase(App);
